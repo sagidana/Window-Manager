61e9f47a8755d1c869a3a427d2ed244698f586c4
Sagi Dana <sagidana1@gmail.com>
2020-05-20 22:02:10 +0000

    adding workspaces to 20

--- wm.h
+++ wm.h
@@ -3,6 +3,7 @@
 
 #include <X11/Xlib.h> 
 #include <X11/keysym.h> 
+#include <X11/XF86keysym.h> 
 
 #include "common.h"
 #include "workspace.h"
@@ -77,54 +78,64 @@ static const char* dmenucmd[] = {   "dmenu_run",
                                     NULL};
 
 static Key wm_keys[] = {
-    {MODKEY,                XK_Return,  spawn,            {.ptr = termcmd,  .i = -1} },
-    {MODKEY,                XK_D,       spawn,            {.ptr = dmenucmd, .i = -1} },
+    {MODKEY,                XK_Return,      spawn,            {.ptr = termcmd,  .i = -1} },
+    {MODKEY,                XK_D,           spawn,            {.ptr = dmenucmd, .i = -1} },
 
     // exit wm
-    {MODKEY | ShiftMask,    XK_E,       to_exit,          {.ptr = NULL,     .i = -1 } },
+    {MODKEY | ShiftMask,    XK_E,           to_exit,          {.ptr = NULL,     .i = -1 } },
 
-    {MODKEY | ShiftMask,    XK_Q,       kill,             {.ptr = NULL,     .i = -1} },
+    {MODKEY | ShiftMask,    XK_Q,           kill,             {.ptr = NULL,     .i = -1} },
 
     // toggle vertical mode
-    {MODKEY,                XK_V,       arrange,          {.ptr = NULL,     .i = XK_V} },
-    {MODKEY | ShiftMask,    XK_V,       arrange,          {.ptr = NULL,     .i = XK_1} },
+    {MODKEY,                XK_V,           arrange,          {.ptr = NULL,     .i = XK_V} },
+    {MODKEY | ShiftMask,    XK_V,           arrange,          {.ptr = NULL,     .i = XK_1} },
     // toggle fullscreen mode
-    {MODKEY,                XK_F,       arrange,          {.ptr = NULL,     .i = XK_F} },
+    {MODKEY,                XK_F,           arrange,          {.ptr = NULL,     .i = XK_F} },
 
     // to move
-    {MODKEY | ShiftMask,    XK_H,       move_left,        {.ptr = NULL,     .i = -1} },
-    {MODKEY | ShiftMask,    XK_J,       move_down,        {.ptr = NULL,     .i = -1} },
-    {MODKEY | ShiftMask,    XK_K,       move_up,          {.ptr = NULL,     .i = -1} },
-    {MODKEY | ShiftMask,    XK_L,       move_right,       {.ptr = NULL,     .i = -1} },
+    {MODKEY | ShiftMask,    XK_H,           move_left,        {.ptr = NULL,     .i = -1} },
+    {MODKEY | ShiftMask,    XK_J,           move_down,        {.ptr = NULL,     .i = -1} },
+    {MODKEY | ShiftMask,    XK_K,           move_up,          {.ptr = NULL,     .i = -1} },
+    {MODKEY | ShiftMask,    XK_L,           move_right,       {.ptr = NULL,     .i = -1} },
 
     // for focus
-    {MODKEY,                XK_H,       focus_left,       {.ptr = NULL,     .i = -1} },
-    {MODKEY,                XK_J,       focus_down,       {.ptr = NULL,     .i = -1} },
-    {MODKEY,                XK_K,       focus_up,         {.ptr = NULL,     .i = -1} },
-    {MODKEY,                XK_L,       focus_right,      {.ptr = NULL,     .i = -1} },
+    {MODKEY,                XK_H,           focus_left,       {.ptr = NULL,     .i = -1} },
+    {MODKEY,                XK_J,           focus_down,       {.ptr = NULL,     .i = -1} },
+    {MODKEY,                XK_K,           focus_up,         {.ptr = NULL,     .i = -1} },
+    {MODKEY,                XK_L,           focus_right,      {.ptr = NULL,     .i = -1} },
 
     // for alignment
-    {MODKEY | Mod1Mask,     XK_H,       arrange,          {.ptr = NULL,     .i = XK_H} },
-    {MODKEY | Mod1Mask,     XK_J,       arrange,          {.ptr = NULL,     .i = XK_J} },
-    {MODKEY | Mod1Mask,     XK_K,       arrange,          {.ptr = NULL,     .i = XK_K} },
-    {MODKEY | Mod1Mask,     XK_L,       arrange,          {.ptr = NULL,     .i = XK_L} },
+    {MODKEY | Mod1Mask,     XK_H,           arrange,          {.ptr = NULL,     .i = XK_H} },
+    {MODKEY | Mod1Mask,     XK_J,           arrange,          {.ptr = NULL,     .i = XK_J} },
+    {MODKEY | Mod1Mask,     XK_K,           arrange,          {.ptr = NULL,     .i = XK_K} },
+    {MODKEY | Mod1Mask,     XK_L,           arrange,          {.ptr = NULL,     .i = XK_L} },
 
     // to resize
-    {MODKEY | ControlMask,  XK_H,       arrange,          {.ptr = NULL,     .i = XK_Y} },
-    {MODKEY | ControlMask,  XK_J,       arrange,          {.ptr = NULL,     .i = XK_U} },
-    {MODKEY | ControlMask,  XK_K,       arrange,          {.ptr = NULL,     .i = XK_I} },
-    {MODKEY | ControlMask,  XK_L,       arrange,          {.ptr = NULL,     .i = XK_O} },
+    {MODKEY | ControlMask,  XK_H,           arrange,          {.ptr = NULL,     .i = XK_Y} },
+    {MODKEY | ControlMask,  XK_J,           arrange,          {.ptr = NULL,     .i = XK_U} },
+    {MODKEY | ControlMask,  XK_K,           arrange,          {.ptr = NULL,     .i = XK_I} },
+    {MODKEY | ControlMask,  XK_L,           arrange,          {.ptr = NULL,     .i = XK_O} },
 
-    {MODKEY,                XK_0,       switch_workspace, {.ptr = NULL,     .i = 0 } },
-    {MODKEY,                XK_1,       switch_workspace, {.ptr = NULL,     .i = 1 } },
-    {MODKEY,                XK_2,       switch_workspace, {.ptr = NULL,     .i = 2 } },
-    {MODKEY,                XK_3,       switch_workspace, {.ptr = NULL,     .i = 3 } },
-    {MODKEY,                XK_4,       switch_workspace, {.ptr = NULL,     .i = 4 } },
-    {MODKEY,                XK_5,       switch_workspace, {.ptr = NULL,     .i = 5 } },
-    {MODKEY,                XK_6,       switch_workspace, {.ptr = NULL,     .i = 6 } },
-    {MODKEY,                XK_7,       switch_workspace, {.ptr = NULL,     .i = 7 } },
-    {MODKEY,                XK_8,       switch_workspace, {.ptr = NULL,     .i = 8 } },
-    {MODKEY,                XK_9,       switch_workspace, {.ptr = NULL,     .i = 9 } }
+    {MODKEY,                XK_1,           switch_workspace, {.ptr = NULL,     .i = 1 } },
+    {MODKEY,                XK_2,           switch_workspace, {.ptr = NULL,     .i = 2 } },
+    {MODKEY,                XK_3,           switch_workspace, {.ptr = NULL,     .i = 3 } },
+    {MODKEY,                XK_4,           switch_workspace, {.ptr = NULL,     .i = 4 } },
+    {MODKEY,                XK_5,           switch_workspace, {.ptr = NULL,     .i = 5 } },
+    {MODKEY,                XK_6,           switch_workspace, {.ptr = NULL,     .i = 6 } },
+    {MODKEY,                XK_7,           switch_workspace, {.ptr = NULL,     .i = 7 } },
+    {MODKEY,                XK_8,           switch_workspace, {.ptr = NULL,     .i = 8 } },
+    {MODKEY,                XK_9,           switch_workspace, {.ptr = NULL,     .i = 9 } },
+    {MODKEY,                XK_0,           switch_workspace, {.ptr = NULL,     .i = 10 } },
+    {MODKEY,                XK_KP_End,      switch_workspace, {.ptr = NULL,     .i = 11 } },
+    {MODKEY,                XK_KP_Down,     switch_workspace, {.ptr = NULL,     .i = 12 } },
+    {MODKEY,                XK_KP_Next,     switch_workspace, {.ptr = NULL,     .i = 13 } },
+    {MODKEY,                XK_KP_Left,     switch_workspace, {.ptr = NULL,     .i = 14 } },
+    {MODKEY,                XK_KP_Begin,    switch_workspace, {.ptr = NULL,     .i = 15 } },
+    {MODKEY,                XK_KP_Right,    switch_workspace, {.ptr = NULL,     .i = 16 } },
+    {MODKEY,                XK_KP_Home,     switch_workspace, {.ptr = NULL,     .i = 17 } },
+    {MODKEY,                XK_KP_Up,       switch_workspace, {.ptr = NULL,     .i = 18 } },
+    {MODKEY,                XK_KP_Prior,    switch_workspace, {.ptr = NULL,     .i = 19 } },
+    {MODKEY,                XK_KP_Insert,   switch_workspace, {.ptr = NULL,     .i = 20 } }
 };
 
 // ------------------------------------------------------------------
